6fcea6588b55416901f79ad1d78784cec4baa628
Amit Banne <Amit Banne>
2017-03-19 20:48:57 -0400

    Deleted bad name directory

--- Page_Rank_in_Map_Reduce/src/counter/PageRankCounter.java
+++
@@ -1,6 +0,0 @@
-package mr.page_rank.counter;
-
-public enum PageRankCounter {
-	PAGE_COUNTER, // CONSTANT for counter to compute total PAGES in dataset
-	DANGLING_NODE_SCORE // CONSTANT for counter to compute total DANGLING_NODES scores in one iteration
-}
